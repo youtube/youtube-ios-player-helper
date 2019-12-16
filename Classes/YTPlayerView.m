1f9bae52eedcb74e82e5b68eea8bf5505eb2a9c1
Vitaly Chupryk <vch@issuu.com>
2019-12-16 18:32:38 +0100

    Load bundle by identifier to let the framework be used properly in Swift

--- Classes/YTPlayerView.m
+++ Classes/YTPlayerView.m
@@ -715,9 +715,9 @@ NSString static *const kYTPlayerSyndicationRegexPattern = @"^https://tpc.googles
   // in case of using Swift and embedded frameworks, resources included not in main bundle,
   // but in framework bundle
   if (!path) {
-      path = [[[self class] frameworkBundle] pathForResource:@"YTPlayerView-iframe-player"
-                                                     ofType:@"html"
-                                                inDirectory:@"Assets"];
+      path = [[[self class] assetsBundle] pathForResource:@"YTPlayerView-iframe-player"
+                                                   ofType:@"html"
+                                              inDirectory:@"Assets"];
   }
     
   NSString *embedHTMLTemplate =
@@ -871,15 +871,17 @@ NSString static *const kYTPlayerSyndicationRegexPattern = @"^https://tpc.googles
   self.webView = nil;
 }
 
-+ (NSBundle *)frameworkBundle {
-    static NSBundle* frameworkBundle = nil;
++ (NSBundle *)assetsBundle {
+    static NSBundle* assetsBundle = nil;
+    
     static dispatch_once_t predicate;
     dispatch_once(&predicate, ^{
-        NSString* mainBundlePath = [[NSBundle bundleForClass:[self class]] resourcePath];
-        NSString* frameworkBundlePath = [mainBundlePath stringByAppendingPathComponent:@"Assets.bundle"];
-        frameworkBundle = [NSBundle bundleWithPath:frameworkBundlePath];
+        NSBundle *frameworkBundle = [NSBundle bundleWithIdentifier:@"org.cocoapods.youtube-ios-player-helper"];
+        NSString *assetsBundlePath = [frameworkBundle pathForResource:@"Assets" ofType:@"bundle"];
+        assetsBundle = [NSBundle bundleWithPath:assetsBundlePath];
     });
-    return frameworkBundle;
+
+    return assetsBundle;
 }
 
 @end
